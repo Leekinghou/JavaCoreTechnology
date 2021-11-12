8f211024db941ed806b88a578f2011e3709ffcd7
leekinghou <leekinghou@gmail.com>
2021-11-12 12:16:38 +0800

    :flag: Java practise

--- src/main/java/v1ch02/RoadApplet.java
+++ src/main/java/v1ch02/RoadApplet.java
@@ -1,13 +1,50 @@
 package v1ch02;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author: lijinhao
- * @date: 2021/10/27 20:35
+ * @date: 2021/11/12 10:59
  */
-public class RoadApplet extends JApplet{
+public class RoadApplet extends JApplet {
     private RoadComponent roadComponent;
+    private JSlider slowdown;
+    private JSlider arrival;
 
+    public void init(){
+        EventQueue.invokeLater(() ->{
+            roadComponent = new RoadComponent();
+            slowdown = new JSlider(0, 100, 10);
+            arrival = new JSlider(0, 100, 50);
 
+            JPanel p = new JPanel();
+            p.setLayout(new GridLayout(1, 6));
+            p.add(new JLabel("Slowdown"));
+            p.add(slowdown);
+            p.add(new JLabel(""));
+            p.add(new JLabel("Arrival"));
+            p.add(arrival);
+            p.add(new JLabel(""));
+            setLayout(new BorderLayout());
+            add(p, BorderLayout.NORTH);
+            add(roadComponent, BorderLayout.CENTER);
+        });
+    }
+    public void start()
+    {
+        new Thread(() ->
+        {
+            for (;;)
+            {
+                roadComponent.update(
+                        0.01 * slowdown.getValue(),
+                        0.01 * arrival.getValue());
+                try {
+                    Thread.sleep(50);
+                }
+                catch(InterruptedException e) {}
+            }
+        }).start();
+    }
 }
